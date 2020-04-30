268f9828208ff86c7380d0567af58d82b4bbfb84
mr-create <64602876+mr-create@users.noreply.github.com>
2020-04-30 18:24:47 +0100

    Revert "Pulls sample data from a JSON generator"
    This reverts commit 3bee7d0152856149a5808acdecaad8dfbd8c6ae3.

--- AngularJS_JSON/scripts/main.js
+++ AngularJS_JSON/scripts/main.js
@@ -8,10 +8,30 @@ $scope.error = null;
 $scope.getOrders = function() {
     $scope.loading = true;
     $http({
-        method: "Get", url: 'http://www.json-generator.com/api/json/get/cgyOuQBrxe?indent=2'
+        method: "Get", url: 'http://localhost:8000/getorders'
     }).then(function(response) {
         $scope.loading = false;
         $scope.orders = response.data;
+
+    }, function(reason) {
+        $scope.loading = false;
+        if(reason.status == -1){
+            $scope.error = 'Unable to extract customers data. Error:\'Unable To Connect To The Server\'';
+        }else{
+        $scope.error = 'Unable to extract customers data. Error:\'' + reason.status +' '+ reason.statusText + '\'';
+        }
+    });
+    $scope.loading == null;
+}
+$scope.getOrderDetails = function(orderID) {
+    $scope.loading = true;
+    $scope.orderURL = `http://localhost:8000/getorderdetailsbyorderid/${orderID}`;
+    $scope.orderDetails = null;
+    $http({
+        method: "Get", url: $scope.orderURL
+    }).then(function(response) {
+        $scope.loading = false;
+        $scope.orderDetails = response.data;
 
     }, function(reason) {
         $scope.loading = false;
